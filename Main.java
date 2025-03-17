1925574d0b2878b6ffcf43402f1ed993dd9561dd
chae909 <jeongyoon2004@naver.com>
2025-03-17 11:44:14 +0900

    edit

--- Main.java
+++ Main.java
@@ -1,8 +1,6 @@
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
